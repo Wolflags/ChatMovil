2fc89a32e6d1d677412c3470265655587171c465
wolflags <joserafaelrey360@gmail.com>
2024-09-28 14:36:14 -0400

    Logica login y register

--- app/src/main/java/com/pucmm/chatmovil/Login.java
+++ app/src/main/java/com/pucmm/chatmovil/Login.java
@@ -5,17 +5,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
 
     Button loginButton;
+    Button registerButton;
+    EditText loginEmailField;
+    EditText loginPasswordField;
     ProgressBar progressBar;
 
     @Override
@@ -29,14 +36,52 @@ public class Login extends AppCompatActivity {
             return insets;
         });
         loginButton = findViewById(R.id.login_ingresar_button);
+        registerButton = findViewById(R.id.login_registrar_button);
         progressBar = findViewById(R.id.login_loading_progress);
+        loginEmailField = findViewById(R.id.login_email_field);
+        loginPasswordField = findViewById(R.id.login_password_field);
 
         progressBar.setVisibility(View.GONE);
 
+        setup();
+
+    }
+
+    private void setup() {
         loginButton.setOnClickListener(view -> {
+            if (loginEmailField.getText().toString().isEmpty() || loginPasswordField.getText().toString().isEmpty()) {
+                return;
+            }
             progressBar.setVisibility(View.VISIBLE);
             loginButton.setEnabled(false);
+            new Handler().postDelayed(() -> {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(loginEmailField.getText().toString(), loginPasswordField.getText().toString()).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(Login.this, Home.class));
+                        finish();
+                    } else {
+                        showError(task.getException().getMessage());
+                        progressBar.setVisibility(View.GONE);
+                        loginButton.setEnabled(true);
+                    }
+                });
 
+
+            }, 1000);
+        });
+
+        registerButton.setOnClickListener(view -> {
+            startActivity(new Intent(Login.this, Register.class));
         });
     }
+
+    private void showError(String error){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage(error);
+        builder.setPositiveButton("Aceptar", null);
+        builder.create().show();
+
+    }
+
 }

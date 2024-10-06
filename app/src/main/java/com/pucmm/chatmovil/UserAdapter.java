9efd9955c7b35aae4ceb35792a3977d1c9cf69f5
Demian Reynoso <demianreynosogomez@gmail.com>
2024-10-05 22:31:52 -0400

    Agregando clase UserAdapter

---
+++ app/src/main/java/com/pucmm/chatmovil/UserAdapter.java
@@ -0,0 +1,71 @@
+package com.pucmm.chatmovil;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+    private List<User> users = new ArrayList<>();
+    private OnUserClickListener listener;
+
+    public UserAdapter(OnUserClickListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.bind(user);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+        notifyDataSetChanged();
+    }
+
+    class UserViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameTextView;
+        private TextView emailTextView;
+
+        UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.nameTextView);
+            emailTextView = itemView.findViewById(R.id.emailTextView);
+
+            itemView.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION && listener != null) {
+                    listener.onUserClick(users.get(position));
+                }
+            });
+        }
+
+        void bind(User user) {
+            nameTextView.setText(user.getName());
+            emailTextView.setText(user.getEmail());
+        }
+    }
+
+    public interface OnUserClickListener {
+        void onUserClick(User user);
+    }
+}
